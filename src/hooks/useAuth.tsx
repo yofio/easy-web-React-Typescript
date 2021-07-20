48b9992d57d56a0fde0cd3b53dc659e2ce6cfcbd
yofio <65844842+yofio@users.noreply.github.com>
2021-07-20 14:54:42 +0900

    メッセージ表示機能の実装

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -2,9 +2,12 @@ import axios from "axios";
 import { useCallback, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { User } from "../types/api/user";
+import { useMessage } from "./useMessage";
 
 export const useAuth = () => {
   const history = useHistory();
+  const { showMessage } = useMessage();
+
   const [loading, setLoading] = useState(false);
   const login = useCallback(
     (id: string) => {
@@ -13,15 +16,18 @@ export const useAuth = () => {
         .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
         .then((res) => {
           if (res.data) {
+            showMessage({ title: "ログインしました", status: "success" });
             history.push("/home");
           } else {
-            alert("ユーザーが見つかりません");
+            showMessage({ title: "ユーザーが見つかりません", status: "error" });
           }
         })
-        .catch(() => alert("ログインできません"))
+        .catch(() =>
+          showMessage({ title: "ログインできません", status: "error" })
+        )
         .finally(() => setLoading(false));
     },
-    [history]
+    [history, showMessage]
   );
   return { login, loading };
 };
