9995e8745e10a65e5f74e8d925e78ac1bd2f74bc
yofio <65844842+yofio@users.noreply.github.com>
2021-07-21 20:07:25 +0900

    ユーザー詳細モーダル機能の実装

--- src/components/organisms/user/UserDetailModal.tsx
+++ src/components/organisms/user/UserDetailModal.tsx
@@ -11,14 +11,16 @@ import {
   Stack
 } from "@chakra-ui/react";
 import { memo, VFC } from "react";
+import { User } from "../../../types/api/user";
 
 type Props = {
+  user: User | null;
   isOpen: boolean;
   onClose: () => void;
 };
 
 export const UserDetailModal: VFC<Props> = memo((props) => {
-  const { isOpen, onClose } = props;
+  const { isOpen, onClose, user } = props;
   return (
     <Modal
       isOpen={isOpen}
@@ -34,22 +36,22 @@ export const UserDetailModal: VFC<Props> = memo((props) => {
           <Stack spacing={4}>
             <FormControl>
               <FormLabel>名前</FormLabel>
-              <Input value="ジャケぇ" isReadOnly />
+              <Input value={user?.username} isReadOnly />
             </FormControl>
 
             <FormControl>
               <FormLabel>フルネーム</FormLabel>
-              <Input value="aiauauaau" isReadOnly />
+              <Input value={user?.name} isReadOnly />
             </FormControl>
 
             <FormControl>
               <FormLabel>MAIL</FormLabel>
-              <Input value="asas@asasas" isReadOnly />
+              <Input value={user?.email} isReadOnly />
             </FormControl>
 
             <FormControl>
               <FormLabel>TEL</FormLabel>
-              <Input value="121-12121-1212121" isReadOnly />
+              <Input value={user?.phone} isReadOnly />
             </FormControl>
           </Stack>
         </ModalBody>
--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -10,16 +10,24 @@ import { memo, useCallback, useEffect, VFC } from "react";
 import { UserCard } from "../organisms/user/UserCard";
 import { useAllUsers } from "../../hooks/useAllUsers";
 import { UserDetailModal } from "../organisms/user/UserDetailModal";
+import { useSelectUser } from "../../hooks/useSelectUser";
 
 export const UserManagement: VFC = memo(() => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const { getUsers, loading, users } = useAllUsers();
+  const { onSelectUser, selectedUser } = useSelectUser();
 
   useEffect(() => {
     getUsers();
   }, []);
 
-  const onClickUser = useCallback(() => onOpen(), []);
+  const onClickUser = useCallback(
+    (id: number) => {
+      onSelectUser({ id, users, onOpen });
+      console.log("aaaa");
+    },
+    [users, onSelectUser, onOpen]
+  );
 
   return (
     <>
@@ -36,12 +44,13 @@ export const UserManagement: VFC = memo(() => {
                 useName={user.username}
                 fullName={user.name}
                 onClick={onClickUser}
+                id={user.id}
               />
             </WrapItem>
           ))}
         </Wrap>
       )}
-      <UserDetailModal isOpen={isOpen} onClose={onClose} />
+      <UserDetailModal isOpen={isOpen} onClose={onClose} user={selectedUser} />
     </>
   );
 });
